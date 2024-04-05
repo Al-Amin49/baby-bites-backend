188e8d56033350ee0cce9c311049454bbe421613
Al-Amin <alamin496832@gmail.com>
2024-04-06 04:28:09 +0600

    created product model

---
+++ src/models/product.model.js
@@ -0,0 +1,67 @@
+import mongoose, { Schema } from "mongoose";
+
+const reviewSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    rating: Number,
+    comment: String,
+  },
+  { timestamps: true }
+);
+
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      default:0
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    stockQuantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    brand: {
+      type: String,
+    },
+    flashSale: {
+      type: Boolean,
+      default: false,
+    },
+    salePrice: {
+      type: Number,
+    },
+    saleEndDate: {
+      type: Date,
+    },
+    reviews: [reviewSchema],
+    seoTitle: {
+      type: String,
+    },
+    seoDescription: {
+      type: String,
+    },
+  },
+
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
